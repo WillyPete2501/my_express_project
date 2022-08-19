a79dea537e0b3ad12dd0d25152b510afd6f7b005
WillyPete2501 <108033885+WillyPete2501@users.noreply.github.com>
2022-08-19 10:49:56 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,23 +2,35 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.get('/home', function(req, res) {
-    res.send('get/home functions check')
+app.get('/home', function (req, res) {
+  res.send('get/home functions check')
 })
 
-app.get('/test', function(req, res) {
-    res.send('get/test functions check')
+app.get('/test', function (req, res) {
+  res.send('get/test functions check')
 })
 
-app.get('/about', function(req, res) {
-    res.send('get/about functions check')
+app.get('/about', function (req, res) {
+  res.send('get/about functions check')
 })
 
-app.post('/', function(req, res) {
-    res.send('post functions check')
+app.post('/', function (req, res) {
+  res.send('post functions check')
 })
 
-app.listen(PORT, function() {
-    console.log(`listen functions check ${PORT}`)
+app.get('/:num', (req, res, next) => {
+  const num = req.params.num
+  if (!Number(num)) {
+    next({ status: 400, message: 'Please enter a number!' })
+  } else {
+    res.json({ message: `${num} is a great number.` })
+  }
 })
 
+app.use((err, req, res, next) => {
+  res.status(err.status).json({ error: err })
+})
+
+app.listen(PORT, function () {
+  console.log(`listen functions check ${PORT}`)
+})
