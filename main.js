ff122a1a8e39e652b15bbcad6b116330ec1b7a4c
WillyPete2501 <108033885+WillyPete2501@users.noreply.github.com>
2022-08-19 09:33:34 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.get('/home', function(req, res) {
+    res.send('get/home functions check')
+})
+
+app.get('/test', function(req, res) {
+    res.send('get/test functions check')
+})
+
+app.get('/about', function(req, res) {
+    res.send('get/about functions check')
+})
+
+app.post('/', function(req, res) {
+    res.send('post functions check')
+})
+
+app.listen(PORT, function() {
+    console.log(`listen functions check ${PORT}`)
+})
+
